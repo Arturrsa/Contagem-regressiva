43ee99b018a97c60752458f968a76c325d6d8a2c
artur henrique <arturr.sanntos@gmail.com>
2022-08-05 13:53:09 -0300

    criei uma pagina principal

---
+++ ex/exercícios.py
@@ -0,0 +1,6 @@
+from time import sleep
+print('Iniciando contagem Regressiva a bomba ira explodir em 10 segundos! ')
+for cont in range(10, -1, -1):
+   print(cont)
+   sleep(1)
+print('Bum! Bum! poow! ')
